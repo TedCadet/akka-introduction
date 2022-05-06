dc533adf598a8d89cfc161bdd22bae50ee2eeb37
TedCadet <e_cadet22@hotmail.com>
2022-05-05 20:23:16 -0400

    actors added

---
+++ src/main/scala/actors/GreeterMain.scala
@@ -0,0 +1,27 @@
+package actors
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+
+/**
+ * guardian actor. it will bootstrap all the other actors of the application
+ */
+object GreeterMain {
+  // message
+  final case class SayHello(name: String)
+
+  def apply(): Behavior[SayHello] = {
+    // setup bootstrap the application
+    Behaviors.setup { context =>
+      // create greeter actor
+      val greeter = context.spawn(Greeter(), "greeter")
+
+      Behaviors.receiveMessage { message =>
+        // create greeterBot actor
+        val replyTo = context.spawn(GreeterBot(max = 3), message.name)
+        greeter ! Greeter.Greet(message.name, replyTo)
+        Behaviors.same
+      }
+    }
+  }
+}
