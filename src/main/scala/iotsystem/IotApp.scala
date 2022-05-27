319f5c1b7701c089c7851ecd9d93b2639d0e77b2
TedCadet <e_cadet22@hotmail.com>
2022-05-26 22:50:24 -0400

    IotSupervisor modified to be written ony in a functional style

--- src/main/scala/iotsystem/IotApp.scala
+++ src/main/scala/iotsystem/IotApp.scala
@@ -1,7 +1,8 @@
 package iotsystem
 
+import akka.NotUsed
 import akka.actor.typed.ActorSystem
 
 object IotApp extends App {
-  ActorSystem[Nothing](IotSupervisor(), "iot-system")
+  ActorSystem[NotUsed](IotSupervisor(), "iot-system")
 }
--- src/main/scala/iotsystem/IotSupervisor.scala
+++ src/main/scala/iotsystem/IotSupervisor.scala
@@ -1,26 +1,38 @@
 package iotsystem
 
+import akka.NotUsed
 import akka.actor.typed.{Behavior, PostStop, Signal}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 
 object IotSupervisor {
-  def apply(): Behavior[Nothing] = Behaviors.setup[Nothing](
-    context => new IotSupervisor(context)
-  )
+  //  def apply(): Behavior[Nothing] = Behaviors.setup[Nothing](
+  //    context => new IotSupervisor(context)
+  //  )
+  def apply(): Behavior[NotUsed] = Behaviors.setup {
+    (context) => {
+      context.log.info("IoT Application started")
+
+      Behaviors.receiveSignal {
+        case (context, PostStop) =>
+          context.log.info("Iot Application stopped")
+          Behaviors.same
+      }
+    }
+  }
 }
 
-class IotSupervisor(context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](context) {
-  context.log.info("IoT Application started")
-
-  override def onMessage(msg: Nothing): Behavior[Nothing] = {
-    // No need to handle any messages
-    Behaviors.unhandled
-  }
-
-  override def onSignal: PartialFunction[Signal, Behavior[Nothing]] = {
-    case PostStop =>
-      context.log.info("Iot Application stopped")
-      this
-  }
-
-}
+//class IotSupervisor(context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](context) {
+//  context.log.info("IoT Application started")
+//
+//  override def onMessage(msg: Nothing): Behavior[Nothing] = {
+//    // No need to handle any messages
+//    Behaviors.unhandled
+//  }
+//
+//  override def onSignal: PartialFunction[Signal, Behavior[Nothing]] = {
+//    case PostStop =>
+//      context.log.info("Iot Application stopped")
+//      this
+//  }
+//
+//}
